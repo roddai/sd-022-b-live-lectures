21a1f759bdfc0e1e8cc8d31b3d02be89fe13c9ca
Rodrigo <rodrigodarosa@MacBook-Air-de-Rodrigo.local>
2022-04-06 08:59:05 -0300

    Trabalhando com condicionais

---
+++ conditionals.js
@@ -0,0 +1,20 @@
+// Primeira parte
+let number1 = 60;
+let number2 = 55;
+
+if (number1 > number2) {
+  console.log('The first number is bigger!');
+} else {
+  console.log('The second number is bigger!');
+}
+
+// Segunda parte
+let age = 18;
+
+if (age > 18) {
+  console.log('Can drive');
+} else if (age < 18) {
+  console.log('Can not drive');
+} else {
+  console.log('Can drive, but with restrictions');
+}
