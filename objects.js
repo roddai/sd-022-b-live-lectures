a115b5bf9b2b12b2774e89d0c357460f5f91c437
Rodrigo <rodrigodarosa@MacBook-Air-de-Rodrigo.local>
2022-04-11 08:37:10 -0300

    Exemplos com objetos

--- objects.js
+++ objects.js
@@ -1,11 +1,3 @@
-// let firstName = 'Joana';
-// let lastName = 'Pereira';
-// let agePerson = 43;
-// let canDrive = true;
-// let favoriteMovies = ['Spider Man', 'Matrix'];
-// let favoriteNumbers = [3, 4, 6];
-// let favoriteAlbuns = ['Skank'];
-
 let person = {
   firstName: 'Joana',
   lastName: 'Pereira',
@@ -15,3 +7,35 @@ let person = {
   favoriteNumbers: [3, 4, 6],
   favoriteAlbuns: ['Skank'],
 };
+
+// Dot notation (notação por ponto)
+person.firstName = 'Cassia';
+person.city = 'São Paulo';
+
+// console.log('Name:', person);
+// console.log('Name:', person.firstName);
+// console.log('Age:', person.agePerson);
+// console.log('Favorite Movie 1:', person.favoriteMovies[0]);
+// console.log('Favorite Movie 2:', person.favoriteMovies[1]);
+
+// Bracket Notation (notação por colchetes)
+person['favoriteFood'] = 'Doritos';
+person['firstName'] = 'Rita';
+person['user'] = ['user1', 'user2', 'user3'];
+
+// Adiciona objeto
+person['address'] = {
+  street: 'Rua XYZ',
+  number: 345,
+}
+
+delete person.city;
+
+// console.log(person);
+// console.log(person.address.street);
+// console.log(person.address['street']);
+// console.log(person['address']['street']);
+
+for (let key in person) {
+  console.log('Key:', key, '-->', 'Value:', person[key]);
+}
