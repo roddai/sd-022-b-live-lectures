aabde51351ee33f91871828e6f2f3da131fd09e2
Rodrigo <rodrigodarosa@MacBook-Air-de-Rodrigo.local>
2022-04-11 08:00:14 -0300

    Criação do arquivo objects.js e exemplo de objeto

---
+++ objects.js
@@ -0,0 +1,17 @@
+// let firstName = 'Joana';
+// let lastName = 'Pereira';
+// let agePerson = 43;
+// let canDrive = true;
+// let favoriteMovies = ['Spider Man', 'Matrix'];
+// let favoriteNumbers = [3, 4, 6];
+// let favoriteAlbuns = ['Skank'];
+
+let person = {
+  firstName: 'Joana',
+  lastName: 'Pereira',
+  agePerson: 43,
+  canDrive: true,
+  favoriteMovies: ['Spider Man', 'Matrix'],
+  favoriteNumbers: [3, 4, 6],
+  favoriteAlbuns: ['Skank'],
+};
