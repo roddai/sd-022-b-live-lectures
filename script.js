ca79b2611fe60571ceab7b12f94b2f1b04cd5f6d
Rodrigo <rodrigodarosa@MacBook-Air-de-Rodrigo.local>
2022-04-14 09:32:47 -0300

    Refatorando o código - parte 2

--- script.js
+++ script.js
@@ -9,28 +9,13 @@ function createDivElement(classes) {
   return newDiv;
 }
 
-// let newDiv1 = document.createElement('div');
-// newDiv1.className = 'square yellow small';
-
-// let newDiv2 = document.createElement('div');
-// newDiv2.className = 'circle red big';
-
-// let newDiv3 = document.createElement('div');
-// newDiv3.className = 'square green medium';
-
 // 3. Adiciono o elemento criado na estrutura HTML
 function addElementAsChild(parent, element) {
   parent.appendChild(createDivElement(element));
 }
 
-addElementAsChild(container, 'square yellow small');
-addElementAsChild(container, 'circle red small');
-addElementAsChild(container, 'square green medium');
+let features = ['square yellow small', 'circle red small', 'square green medium'];
 
-// container.appendChild(createDivElement('square yellow small'));
-// container.appendChild(createDivElement('circle red small'));
-// container.appendChild(createDivElement('square green medium'));
-
-// container.appendChild(newDiv2);
-// container.appendChild(newDiv3);
-
+for (let fetClasses of features) {
+  addElementAsChild(container, fetClasses);
+}
