7bee1002368cbd2d38c75cac01634e50179c7c1e
Rodrigo <rodrigodarosa@MacBook-Air-de-Rodrigo.local>
2022-04-18 09:01:45 -0300

    Iniciando a construção do código para a corrida dos carrinhos

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
 // window.onload = function () {
 //   console.log('Hello!');
 // } 
-window.onload = function () {
-  console.log('palmas');
-}
 
-function func1() {
-  console.log('café');
-}
-
-func1();
-
-
-// window.onload = function () {  // função anônima
-//   let button = document.getElementById('start-race-btn');
-
-//   let car1 = document.querySelector('.car1');
-//   let car2 = document.querySelector('.car2');
-
-//   function resetCars(carroA, carroB) {
-//     carroA.style.marginLeft = 0;
-//     carroB.style.marginLeft = 0;
-//   }
-
-//   resetCars(car1, car2);
-
-//   // 1- Qual o tipo de evento devo escutar?
-//   // 2 - O que eu faço quando esse evento acontecer?
-//   button.addEventListener('click', function () {
-//     car1.style.marginLeft = parseInt(car1.style.marginLeft) + (Math.random() * 100) + 'px';
-//     car2.style.marginLeft = parseInt(car2.style.marginLeft) + (Math.random() * 100) + 'px';
-
-//     if (parseInt(car1.style.marginLeft) > window.innerWidth){
-//       alert('Carro 1 ganhou!!!!');
-//       resetCars(car1, car2);
-//     }
-
-//     if (parseInt(car2.style.marginLeft) > window.innerWidth){
-//       alert('Carro 2 ganhou!!!!');
-//       resetCars(car1, car2);
-//     }
-//   });
+// window.onload = function () {
+//   console.log('palmas');
 // }
+
+// function func1() {
+//   console.log('café');
+// }
+
+// func1();
+
+window.onload = function () {
+  let button = document.getElementById('start-race-btn');
+  // 1. qual o tipo de evento devo executar?
+  // 2. o que faço quando este evento acontecer?
+  let car1 = document.querySelector('.car1');
+  let car2 = document.querySelector('.car2');
+
+  car1.style.marginLeft = 0;
+  car2.style.marginLeft = 0;
+
+  // button.addEventListener(1, 2);
+  button.addEventListener('click', function () {
+    car1.style.marginLeft = parseInt(car1.style.marginLeft) + (Math.random() * 100) + 'px';
+    car2.style.marginLeft = parseInt(car2.style.marginLeft) + (Math.random() * 100) + 'px';
+
+    
+    // car2.style.marginLeft = '200px';
+
+  // Vamos usar em alguns projetos
+    // console.log(event); // evento com todos os dados do elemento criado
+    // console.log(event.target); // captura o elemento clicado
+    // console.log(event.target.innerHTML); // captura o texto do elemento clicado
+    // console.log(event.target.id); // captura o id do elemento clicado
+    // console.log(event.target.className);// captura a classe do elemento clicado
+  });
+}
