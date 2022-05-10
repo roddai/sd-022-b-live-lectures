73001d7c5e14f1bdb7a8ed5bb44350bbce798e18
Rodrigo <rodrigodarosa@MacBook-Air-de-Rodrigo.local>
2022-05-10 08:21:08 -0300

    Substituindo test por it

--- script.test.js
+++ script.test.js
@@ -1,42 +1,42 @@
 const { sum, summationOf } = require('./script');
 
 describe('Requisito 1 - Testes para a função sum', () => {
-  test('Soma 1 + 2 para igualar a 3', () => {
+  it('Soma 1 + 2 para igualar a 3', () => {
     // expect().matcher()
     expect(sum(1, 2)).toBe(3);
   });
   
-  test('Soma 5.1 + 6.55 para igualar a 11.65', () => {
+  it('Soma 5.1 + 6.55 para igualar a 11.65', () => {
     expect(sum(5.1, 6.55)).toBeCloseTo(11.65);
   });
   
-  test('Soma 0 + 0 para igualar a 0', () => {
+  it('Soma 0 + 0 para igualar a 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
   
-  test('Soma -5 + (-39)) para igualar a -44', () => {
+  it('Soma -5 + (-39)) para igualar a -44', () => {
     expect(sum(-5, -39)).toBe(-44);
   });
   
-  test('Soma 0 + (-10)) para igualar a -10', () => {
+  it('Soma 0 + (-10)) para igualar a -10', () => {
     expect(sum(0, -10)).toBe(-10);
   });
 })
 
 describe('Requisito 2 - Testes para a função summationOf', () => {
-  test('Espera que a função summationOf exista', () => {
+  it('Espera que a função summationOf exista', () => {
     expect(typeof summationOf).toBe('function');
   })
   
-  test('Espera-se que o somatório de 1 seja igual a 1', () => {
+  it('Espera-se que o somatório de 1 seja igual a 1', () => {
     expect(summationOf(1)).toBe(1);
   })
   
-  test('Espera-se que o somatório de 3 seja igual a 6', () => {
+  it('Espera-se que o somatório de 3 seja igual a 6', () => {
     expect(summationOf(3)).toBe(6);
   })
   
-  test('Espera-se que o somatório de 5 seja igual a 15', () => {
+  it('Espera-se que o somatório de 5 seja igual a 15', () => {
     expect(summationOf(5)).toBe(15);
   })
 });
